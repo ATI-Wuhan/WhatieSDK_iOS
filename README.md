f9ab211b61ec27f838cc6b55ff5c2ddbdefce4f9
Change <change@ChangedeMac-mini.local>
2018-04-13 14:17:55 +0800

    add SDK

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # WhatieSDK
+# hello,eHome
---
+++ WhatieSDK/WhatieSDK.framework/Headers/WhatieSDK.h
@@ -0,0 +1,21 @@
+//
+//  WhatieSDK.h
+//  WhatieSDK
+//
+//  Created by Change on 2018/4/13.
+//  Copyright © 2018年 zhouwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WhatieSDK.
+FOUNDATION_EXPORT double WhatieSDKVersionNumber;
+
+//! Project version string for WhatieSDK.
+FOUNDATION_EXPORT const unsigned char WhatieSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WhatieSDK/PublicHeader.h>
+
+#import <WhatieSDK/EHOMEString.h>
+
+
